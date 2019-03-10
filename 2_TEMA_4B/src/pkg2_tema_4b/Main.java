a978a41d446557317566ca8c2945947dd3291f63
MyT25 <marco.antonio.noh1999@gmail.com>
2019-03-10 17:27:42 -0600

    cuadros de dialogos

---
+++ 2_TEMA_4B/src/pkg2_tema_4b/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2_tema_4b;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author MARCKO1999
+ */
+public class Main {
+
+   public static void main(String[] args) {
+        
+        frmPrincipal frm = new frmPrincipal();
+        frm.setVisible(true);
+        frm.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    }
+    
+}
